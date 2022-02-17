362c72c46aa88b2284d8bd6c3e205faf7d4ba3d7
francescalb <francesca.l.bleken@sintef.no>
2022-02-17 22:42:38 +0100

    AtomisticStructureParseStrategy returns ase.Atoms in cache, with test

--- oteapi_asmod/strategies/parse.py
+++ oteapi_asmod/strategies/parse.py
@@ -1,15 +1,15 @@
 """Demo strategy class for text/json."""
 # pylint: disable=no-self-use,unused-argument
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, Optional
 
 from ase import Atoms
 from ase.io import read
-from dlite import Collection, Instance, get_collection, storage_path
+from ase.io.jsonio import MyEncoder
 from oteapi.datacache import DataCache
-from oteapi.models import AttrDict, SessionUpdate
+from oteapi.models import SessionUpdate
 from oteapi.plugins import create_strategy
-from pydantic import BaseModel, Extra, Field, HttpUrl
+from pydantic import BaseModel, Extra, Field
 
 if TYPE_CHECKING:
     from typing import Any, Dict
@@ -18,18 +18,11 @@ if TYPE_CHECKING:
 
 
 class SessionUpdateAtomisticParse(SessionUpdate):
-    """Class for returning values from XLSXParse."""
+    """Class for returning values from oteapi-asmod Parse using ASE."""
 
-    symbols: List[str] = Field(
+    cached_atoms: str = Field(
         ...,
-        description="The chemical symbol of each atom in the atomstic structure. "
-        "This determines the dimension number of atoms.",
-    )
-    # atoms.positions is a np.ndarray , condire making custom pydantic validator
-    positions: List[List[float]] = Field(
-        ...,
-        description="Positions of the atoms in cartesian coordinates. Unit: Å. ",
-        unit="Å",
+        description="The key to the ase.Atoms object in the data cache.",
     )
 
 
@@ -38,20 +31,7 @@ class AtomisticParseDataModel(BaseModel):
 
     fileformat: Optional[str] = Field(
         None,
-        description=("Optional to specify format as input to the ase reader."),
-    )
-    returnformat: Optional[str] = Field(
-        "dict",
-        description=("Optional to specify return type. Default 'dict'"),
-        # Find out how to print alternatives,
-    )
-    datamodeltype: Optional[str] = Field(
-        "dlitedatamodel",
-        description=("Optional to specify type of data model. Default is" "'dlite'"),
-    )
-    datamodel: Optional[HttpUrl] = Field(
-        "http://onto-ns.com/meta/0.1/Molecule",
-        descriptioni=("Optionally specify dlite datamodel to use, for now the uid"),
+        description=("Optional, to specify format as input to the ase reader."),
     )
 
 
@@ -71,20 +51,9 @@ class AtomisticStructureParseStrategy:
             session: A session-specific dictionary context.
 
         Returns:
-            Dictionary of key/value-pairs to be stored in the sessions-specific
-            dictionary context.
+            The session.
 
         """
-        model = AtomisticParseDataModel(
-            **self.parse_config.configuration, extra=Extra.ignore
-        )
-        if model.returntype == "dlitedatamodel" and model.returnformat == "datamodel":
-            if isinstance(session, AttrDict) and "collection_id" in session:
-                coll = get_collection(session["collection_id"])
-            else:
-                coll = Collection()
-                get_collection(coll.uuid)
-            return dict(collection_id=coll.uuid)
         return SessionUpdate
 
     def get(
@@ -99,62 +68,26 @@ class AtomisticStructureParseStrategy:
             session: A session-specific dictionary context.
 
         Returns:
-            Dictionary of key/value-pairs to be stored in the sessions-specific
-            dictionary context.
+            ase.Atoms object in cache.
 
         """
         model = AtomisticParseDataModel(  # Checkout how to do this only once
             **self.parse_config.configuration, extra=Extra.ignore
         )
 
-        storage_path.append(str("/app/entities"))
-
         downloader = create_strategy("download", self.parse_config)
         output = downloader.get()
         cache = DataCache(self.parse_config.configuration)
         content = cache.get(output["key"])
+
         if isinstance(content, Atoms):
-            return content
+            return SessionUpdateAtomisticParse(cached_atoms=output["key"])
 
         name = self.parse_config.downloadUrl.path.rsplit("/")[-1].split(".")
         with cache.getfile(
             key=output["key"], suffix=name[-1], prefix=name[0]
         ) as filename:
             atoms = read(filename, format=model.fileformat)
+        key = cache.add(atoms, json_encoder=MyEncoder)
 
-        if model.returnformat == "datamodel":
-            if model.datamodeltype == "dlitedatamodel":
-                storage_path.append(str("/app/entities"))
-
-                moleculemodel = Instance(  # Need to fix storagepath
-                    "json:///app/entities/Molecule.json"
-                )  # DLite Metadata
-
-                if isinstance(session, AttrDict) and "collection_id" in session:
-                    coll = get_collection(session["collection_id"])
-
-                else:
-                    coll = Collection()
-                    get_collection(coll.uuid)
-
-                basename = ".".join(name)
-
-                inst = moleculemodel(
-                    dims=[len(atoms), 3], id=basename
-                )  # DLite instance
-                inst.symbols = atoms.get_chemical_symbols()
-                inst.masses = atoms.get_masses()
-                inst.positions = atoms.positions
-
-                inst.groundstate_energy = 0.0
-
-                coll.add(label=basename, inst=inst)
-                # Return uuid of the collection that now includes the new parsed
-                # molecule.
-
-                #  return SessionUpdateAtomisticParse(
-                #    data={"collection_id": coll.uuid, "molecule_name": basename}
-                # )
-        return SessionUpdateAtomisticParse(
-            symbols=atoms.get_chemical_symbols(), positions=atoms.positions.tolist()
-        )
+        return SessionUpdateAtomisticParse(cached_atoms=key)
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -5,7 +5,7 @@ def test_xyz_read(repo_dir: "Path") -> None:
     """Test `chemical/x-xyz` parse strategy."""
     from ase.io import read
     from oteapi.models.resourceconfig import ResourceConfig
-
+    from oteapi.datacache import DataCache
     from oteapi_asmod.strategies.parse import AtomisticStructureParseStrategy
 
     filepath = repo_dir / "tests" / "testfiles" / "Ethane.xyz"
@@ -17,9 +17,9 @@ def test_xyz_read(repo_dir: "Path") -> None:
         ),
         mediaType="chemical/x-xyz",
     )
+    cache=DataCache()
     parser = AtomisticStructureParseStrategy(config)
-    parsedatoms = parser.get()
-
+    parsed_atoms_key = parser.get().cached_atoms
+    parsed_atoms = cache.get(parsed_atoms_key)
     atoms = read(filepath)
-    assert parsedatoms.symbols == atoms.get_chemical_symbols()
-    assert parsedatoms.positions == atoms.positions.tolist()
+    assert atoms == parsed_atoms
